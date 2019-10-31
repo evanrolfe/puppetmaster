b094fc1cfa74d349cb8fb34160a499a72d60da81
Evan Rolfe <evanrolfe@gmail.com>
2019-10-31 09:02:57 +0000

    Frontend: still working on preview

--- app/components/RequestView/Preview.js
+++ app/components/RequestView/Preview.js
@@ -30,19 +30,15 @@ export default class Preview extends Component<Props> {
     this._setBody();
   }
 
-  async _setBody() {
+  _setBody() {
     const content = this.props.value;
-    // const contentEncoded = Buffer.from(content).toString('base64');
-    const src = `data:text/html;charset=utf-8,${encodeURIComponent(content)}`;
-    console.log(this.props.request.url);
-    try {
-      // See: https://github.com/electron/electron/issues/17526
-      await this.webViewRef.loadURL(src, { baseURL: this.props.request.url });
-    } catch (err) {
-      console.log(err);
-    }
 
-    // this.webViewRef.webContents = this.webViewRef;
+    // TODO: Why does adding this option in not work?
+    // {baseURLForDataURL: 'http://localhost:3001/'}
+    // See: https://github.com/electron/electron/issues/20700
+    this.webViewRef.loadURL(
+      `data:text/html; charset=UTF-8,${encodeURIComponent(content)}`
+    );
   }
 
   setWebViewRef(ref) {
