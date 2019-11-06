786d2815b7dfdc69b4c8f53e746ba34e02f62c4e
Evan Rolfe <evanrolfe@gmail.com>
2019-11-06 16:44:14 +0000

    Frontend: load/read settings from file

--- app/App.js
+++ app/App.js
@@ -5,6 +5,7 @@ import { createHashHistory } from 'history';
 import { Route } from 'react-router';
 import { HashRouter } from 'react-router-dom';
 import log from 'electron-log';
+import fs from 'fs';
 
 import { registerModal, showModal } from './components/modals/index';
 import AlertModal from './components/modals/AlertModal';
@@ -22,6 +23,8 @@ import ScansPage from './components/pages/ScansPage';
 import BackendConnection from './lib/BackendConnection';
 import SettingsContext from './lib/SettingsContext';
 
+const SETTINGS_FILE = './settings.json';
+
 export default class App extends Component {
   constructor() {
     super();
@@ -38,26 +41,36 @@ export default class App extends Component {
     const backendConnected = backendConn.init();
     global.backendConn = backendConn;
 
-    const selectedColumns = [
-      'id',
-      'method',
-      'host',
-      'path',
-      'response_status',
-      'request_type'
-    ];
-    const requestsTableColumns = ALL_TABLE_COLUMNS.filter(column =>
-      selectedColumns.includes(column.key)
-    );
+    // Load settings from file
+    let settings;
+    if (fs.existsSync(SETTINGS_FILE)) {
+      const settingsJSON = fs.readFileSync(SETTINGS_FILE, 'utf8');
+      settings = JSON.parse(settingsJSON);
 
-    this.state = {
-      settings: {
+      // Or use the default settings
+    } else {
+      const selectedColumns = [
+        'id',
+        'method',
+        'host',
+        'path',
+        'response_status',
+        'request_type'
+      ];
+      const requestsTableColumns = ALL_TABLE_COLUMNS.filter(column =>
+        selectedColumns.includes(column.key)
+      );
+      settings = {
         activeTheme: 'default',
         browserNetworkOrientation: 'vertical',
         requestsTableColumns: requestsTableColumns,
         paneWidth: 700,
         paneHeight: 350
-      },
+      };
+    }
+
+    this.state = {
+      settings: settings,
       // Ensure that the app gets re-rendered once we connect to the backend
       // eslint-disable-next-line react/no-unused-state
       backendConnected: backendConnected
@@ -71,6 +84,23 @@ export default class App extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (this.state.settings.activeTheme !== prevState.settings.activeTheme) {
       this.setTheme();
+    }
+
+    // If the settings have changed:
+    if (
+      this.state.settings.activeTheme !== prevState.settings.activeTheme ||
+      this.state.settings.browserNetworkOrientation !==
+        prevState.settings.browserNetworkOrientation ||
+      this.state.settings.requestsTableColumns !==
+        prevState.settings.requestsTableColumns ||
+      this.state.settings.paneWidth !== prevState.settings.paneWidth ||
+      this.state.settings.paneHeight !== prevState.settings.paneHeight
+    ) {
+      fs.writeFileSync(
+        SETTINGS_FILE,
+        JSON.stringify(this.state.settings, null, 2)
+      );
+      console.log(`Saved settings to ${SETTINGS_FILE}`);
     }
   }
 
