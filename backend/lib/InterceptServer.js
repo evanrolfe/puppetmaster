eec7011c179b8bff4b37bfd0008f04aed842079a
Evan Rolfe <evanrolfe@gmail.com>
2019-12-05 10:01:58 +0000

    Backend: comment

--- backend/lib/InterceptServer.js
+++ backend/lib/InterceptServer.js
@@ -4,6 +4,17 @@ const mainIpc = require('../server-ipc');
 
 const ipc = new RawIPC();
 
+/*
+ * InterceptServer: communicates with the BrowserInterceptPage on the frontend.
+ * It starts an ipc server on the channel "intercept".
+ * When a request is intercepted it first sends a "requestIntercepted" message
+ * to the client on the mainIpc channel. It then waits until the a decision
+ * message is received on the intercept channel and it will then either
+ * forward the request or drop it depending on the message. If a new request is
+ * queued while the first one is still waiting for the message from the client,
+ * it will wait until that first request has completed to send a new
+ * "requestIntercepted" message to the client. And the process repeats...
+ */
 class InterceptServer {
   constructor() {
     this.events = new EventEmitter();
